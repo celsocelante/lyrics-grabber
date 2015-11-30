3dc128f998c55c1e9f4a0be02f0d925c8f4a0aa9
Celso Junior <celsojr@inf.ufes.br>
2015-11-30 15:50:10 -0500

    Changes to make it work again with more than one soruce

--- django_project/api/grabber.py
+++ django_project/api/grabber.py
@@ -10,13 +10,11 @@ def encoded_dict(in_dict):
         if isinstance(v, unicode):
             v = v.encode('utf8')
         elif isinstance(v, str):
-            # Must be encoded in UTF-8
             v.decode('utf8')
         out_dict[k] = v
     return out_dict
 
 def make_url_vaga(artist, song):
-	#artist = iri_to_uri(artist)
 	try:
 		data = {}
 		data['artist'] = artist
@@ -99,78 +97,6 @@ def fetch_lyrics_terra(artist, song):
 	except:
 		return None, None
 
-'''def fetch_lyrics_az(artist, song):
-	# Construct the url
-	artist = artist.replace(" ","").lower()
-	song = song.replace(" ", "").lower()
-
-	url = "http://www.azlyrics.com/lyrics/%s/%s.html" % (artist, song)
-
-	# Get url content's and search
-	page = requests.get(url)
-	lyrics = re.search(b'<!-- start of lyrics -->(?:\r\n)+(.+)(?:\r\n)+<!-- end of lyrics -->', page.content, re.DOTALL)
-
-	if lyrics:
-		return re.sub(r'<.+>', '', lyrics.group(1).decode('utf8'))
-	else:
-		return None'''
-
-'''def fetch_lyrics_chart(artist, song):
-	artist = artist.lower()
-	song = song.lower()
-
-	data = {}
-	data['artist'] = artist
-	data['song'] = song
-
-	url_values = urllib.urlencode(data)
-	url = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist={}&song={}".format(artist,song)
-
-	try:
-		page = requests.get(url)
-
-		ctype, charset = page.headers['Content-Type'].split(';')
-		encoding = charset[len(' charset='):]
-
-		id = re.search(b'<LyricId>(.*?)<\/LyricId>', page.content, re.DOTALL)
-		if id == "0":
-			return None
-
-		lyrics = re.search(b'<Lyric>(.*?)<\/Lyric>', page.content, re.DOTALL)
-
-		if lyrics:
-			return lyrics.group(1).replace("<br/>","\n").decode(encoding)
-		else:
-			return None
-	except:
-		return None'''
-
-
-'''def fetch_lyrics_wikia(artist, song):
-	artist = uri_to_iri(artist.replace(" ","_").title())
-	song = song.replace(" ", "_").title()
-	MARKER_NOLYRICS = 'PUT LYRICS HERE'
-	url = "http://lyrics.wikia.com/index.php?title=%s:%s&action=edit" % (artist, song)
-
-	try:
-		page = requests.get(url)
-
-		ctype, charset = page.headers['Content-Type'].split(';')
-		encoding = charset[len(' charset='):]
-
-		lyrics = re.search(b'&lt;lyrics>(.*?)&lt;/lyrics>', page.content, re.DOTALL)
-
-
-		if lyrics.group(1).find(MARKER_NOLYRICS) > -1:
-			return None
-
-		if lyrics:
-			return lyrics.group(1)
-		else:
-			return None
-	except:
-		return None'''
-
 def grab_lyrics(artista, musica):
 	data = {}
 	data['status'] = 'ok'
@@ -179,13 +105,11 @@ def grab_lyrics(artista, musica):
 
 	database = Lyrics.objects.filter(artist__icontains=artista,song__icontains=musica)[:1]
 
-	if database.count() is 1:
+	if database.count() > 0:
 		data['text'] = database[0].text
 		data['source'] = 'database'
 		data['url'] = database[0].url
 		return json.dumps(data)
-
-
 
 	vaga, url = fetch_lyrics_vaga(artista, musica)
 
@@ -197,7 +121,18 @@ def grab_lyrics(artista, musica):
 		lyric_object.save()
 
 		return json.dumps(data)
-	
+
+    terra, url = fetch_lyrics_terra(artista, musica)
+
+	if terra is not None:
+		data['text'] = terra
+		data['url'] = url
+
+		lyric_object = Lyrics(artist=artista, song=musica, text=vaga)
+		lyric_object.save()
+
+		return json.dumps(data)
+
 	data['text'] = "not found"
 	data['status'] = '404'
 	data['source'] = None
